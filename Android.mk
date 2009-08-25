1980a39cb422c9bba6f042f55d47eeec2d56f145
Andrei Dolnikov <adolnikov@embeddedalley.com>
2009-08-25 08:47:45 -0400

    Build gnonlin only if BUILD_WITH_GST option provided.
    Signed-off-by: Andrei Dolnikov <adolnikov@embeddedalley.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(strip $(BUILD_WITH_GST)),true)
+
 LOCAL_PATH := $(call my-dir)
 
 GNONLIN_TOP := $(LOCAL_PATH)
@@ -5,3 +7,5 @@ GNONLIN_TOP := $(LOCAL_PATH)
 include $(CLEAR_VARS)
 
 include $(GNONLIN_TOP)/gnl/Android.mk
+
+endif
